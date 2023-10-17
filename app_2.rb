7fb3e56d02f97fafdfe0d430653c769a2b2aad64
ho757uff <robindebessel.pro@gmail.com>
2023-10-17 20:31:22 +0200

    fight flow v2 - working for 2 opponents

--- app_2.rb
+++ app_2.rb
@@ -2,7 +2,7 @@ require "bundler"
 Bundler.require
 
 require_relative "lib/game"
-require_relative "lib/player"
+require_relative "lib/player_2"
 
 # --- Création des joueurs
 
@@ -11,9 +11,7 @@ player1 = HumanPlayer.new("Ta Brute surpuissante")
 ai_opponents_array = []
 
 ai_opponents_array << player2 = Player.new("Grand-père Yves dit 'la p'tite frappe'")
-ai_opponents_array << player3 = Player.new("Grand-mère Josette dite 'l'agressive'")
-
-
+ai_opponents_array << player3 = Player.new("Grand-mère Josette dite 'la passive agressive'")
 
 # player4 = Player.new("Oncle_Pascal_AKA_le_bourrin")
 # player5 = Player.new("Cousin_Jean-Michel_AKA_le_boulet")
@@ -24,80 +22,137 @@ ai_opponents_array << player3 = Player.new("Grand-mère Josette dite 'l'agressiv
 def welcome_screen
   puts "\n\n"
   puts "           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  puts "           |  Bienvenue sur 'ILS VEULENT TOUS MA POO' !      |"
-  puts "           |             (PEGI 13 ans et demis)              |"
-  puts "           |_________________________________________________|"
-  puts "           |  Le but du jeu est de tous bien les niquer !    |"
-  puts "           |      On va voir si t'en as dans le futal !      |"
+  puts "           |                   Bienvenue sur                |"
+  puts "           |                                                |"
+  puts "           |            'ILS VEULENT TOUS MA POO'           |"
+  puts "           |                       II                       |"
+  puts "           |             (PEGI 15 ans et demis )            |"
+  puts "           |________________________________________________|"
+  puts "           |  Le but du jeu est de tous bien les niquer !   |"
+  puts "           |      On va voir si t'en as dans le futal !     |"
   puts "           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 end
 
 # --- Warmup
 
-def combat_warmup(player1, player2)
-  puts "\nLadies & Gentlemen! Bienvenue pour ce combat d'anthologie qui opposera...\n"
-  puts "#{player1.name}, l'indétrônable !"
-  puts "VS"
-  puts "#{player2.name}, l'irrésistible !"
-  puts "\nVoici l'état de chaque joueur avant leur affrontement titanesque :\n"
-  puts "#{player1.show_state}"
-  puts "#{player2.show_state}"
+def combat_warmup(player1, player2, player3)
+  puts "\nLadies & Gentlemen! Bienvenue pour ce combat d'anthologie qui opposera..."
+  puts "\n               #{player1.name}, l'indétrônable !"
+  puts "\n         ~~~~~~~~~~~~~~~~~~>  VS  <~~~~~~~~~~~~~~~~~~"
+  puts "\n#{player2.name} et son acolyte #{player3.name}!"
+  puts "\nVoici l'état de chaque joueur avant leur affrontement titanesque :\n\n"
+  puts "\n\n#{player1.show_state}"
+  puts "\n#{player2.show_state}"
+  puts "#{player3.show_state}"
 
-  puts "\nNOooOwwW ! Let's get READYYY To RrrRRrRrRRRuuUUMMBLLLLLLLLLLLLLLLLLLLLE !"
+  puts "\nNOooOoOooOooooOOooOoOOoWwwwW ! Let's get READYYY To RrrRRrRrRRRuuUUMMBLLLLLLLLLLLLLLLLLLLLE !"
   puts "\n\n"
 end
 
 # --- Round preview
 
-def round_preview(player1, player2)
-  puts "           ~~~~~~~~~~~~~~~~~~~~ NEW ROUND !~~~~~~~~~~~~~~~~~~~~"
-  puts "\n"
-  puts "Q( *.*)D                                                          Q(0_0 Q)"
-  puts "\n"
-  puts "           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+def round_preview(player1, player2, player3)
+  puts "           ~~~~~~~~~~~~~~~~~~~~> NEW ROUND <~~~~~~~~~~~~~~~~~~~~"
+  puts "Q( *.*)D                                                         Q(1_1 Q)  Q(0_0 Q)"
+  puts "           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n\n"
 
-  puts "\nVoici l'état de chaque joueur avant le round :"
+  # puts "\nVoici l'état de chaque joueur avant le round :"
   puts "#{player1.show_state}"
-  puts "#{player2.show_state}"
-
+  # puts "#{player2.show_state}"
+  # puts "#{player3.show_state}"
   puts "\n\n"
-  puts "               ~~~~~~~~~~~~~~~~~~ FIGHT !~~~~~~~~~~~~~~~~~~"
-  puts "\n"
-  puts "                          Q( *.*)=D    <<=(0_0 Q) "
-  puts "\n"
-  puts "               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+  puts "           ~~~~~~~~~~~~~~~~~~~~~~> YOUR TURN <~~~~~~~~~~~~~~~~~~"
+  puts "                                   Q( *.*)=D                     Q(1_1 Q)  Q(0_0 Q) "
+  puts "           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
 end
 
-# --- Combat flow
+def turn_flow(player1, player2, player3, ai_opponents_array)
+  while player1.life_points > 0 && (player2.life_points > 0 || player3.life_points > 0)
+    round_preview(player1, player2, player3)
 
-def combat(player1, player2, player3)
-  while player1.life_points > 0 && (player2.life_points > 0 || player3.life_points > 0) 
-    round_preview(player1)
+    puts "\nQuelle action veux-tu effectuer ce tour-ci ?\n"
+    puts "  A - Chercher une meilleure arme ?        0 - Attaquer #{player2.name} qui a #{player2.life_points} points de vie."
+    puts "  S - Chercher à te soigner ?              1 - Attaquer #{player3.name} qui a #{player3.life_points} points de vie"
+    puts "\n"
 
-    player1.attacks(player2)
-    break if player2.life_points <= 0 # Check if player2 is defeated after player1's attack
+    print "\n> "
+    input = gets.chomp
 
-    player2.attacks(player1)
-    break if player1.life_points <= 0 # Check if player1 is defeated after player2's attack
+    input = input.to_s.upcase
+
+    if input == "A" || input == "S" || input == "0" || input == "1"
+      case input
+      when "A"
+        player1.search_weapon
+      when "S"
+        player1.search_health_pack
+      when "0"
+        player1.attacks(player2)
+      when "1"
+        player1.attacks(player3)
+      end
+    else
+      puts "Sélection invalide. \nTu passes ton tour ! \nChoisis une action VALIDE au tour prochain p'tit NOOBLARD !"
+    end
+
+    puts "\nAppuie sur 'Entrée' pour continuer et lancer le tour des bots !"
+    input = gets.chomp
+
+    puts "               ~~~~~~~~~~~~~~~~~~ BOTS TURN !~~~~~~~~~~~~~~~~~~"
+    puts "Q( *.*)D                  <<==(1_1 Q)  <<==(0_0 Q) "
+    puts "               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+    puts "C'est au tour des bots de jouer ! Ils te maravent ta sale ganache et te lattent tes p'tites fesses !"
+    puts "\n\n"
+    for opponent in ai_opponents_array
+      if opponent.life_points > 0 && player1.life_points > 0
+        opponent.attacks(player1)
+        # if ai_opponents_array[opponent] !
+        puts "Appuie sur 'Entrée' pour passer au bot suivant !"
+        input = gets.chomp
+      end
+    end
+    puts "Appuie sur 'Entrée' pour jouer ton nouveau tour !"
+    input = gets.chomp
   end
-
-  and_the_winner_is(player1, player2)
 end
 
 # --- Winner announcement
 
-def and_the_winner_is(player1, player2)
-  if player1.life_points <= 0
-    puts "\n             Ƹ̵̡Ӝ̵̨̄Ʒ #{player2.name} WINS! Ƹ̵̡Ӝ̵̨̄Ʒ\n\n"
-  elsif player2.life_points <= 0
-    puts "\n             Ƹ̵̡Ӝ̵̨̄Ʒ #{player1.name} WINS! Ƹ̵̡Ӝ̵̨̄Ʒ\n\n"
+def and_the_winner_is(player1, player2, player3)
+  if player2.life_points <= 0 && player3.life_points <= 0
+    puts "\n                       Ƹ̵̡Ӝ̵̨̄Ʒ CONGRATZ, YOU WON! Ƹ̵̡Ӝ̵̨̄Ʒ\n\n"
+  elsif player1.life_points <= 0
+    puts "\n                       Ƹ̵̡Ӝ̵̨̄Ʒ SORRY - YOU LOSE Ƹ̵̡Ӝ̵̨̄Ʒ\n\n"
   end
-  puts "                               ¯\\_(ツ)_/¯\n\n" 
+  puts "                               ¯\\_(ツ)_/¯\n\n"
 end
 
 # --- Game flow
-welcome_screen
-combat_warmup(player1, player2)
-combat(player1, player2)
 
-# binding.pry
+welcome_screen
+combat_warmup(player1, player2, player3)
+# combat(player1, player2, player3)
+turn_flow(player1, player2, player3, ai_opponents_array)
+and_the_winner_is(player1, player2, player3)
+
+# ---
+# ---
+# ---
+# ---
+# ---
+
+# --- Old Combat flow
+
+# def combat(player1, player2, player3)
+#   while player1.life_points > 0 && (player2.life_points > 0 || player3.life_points > 0)
+#     round_preview(player1, player2, player3)
+
+#     player1.attacks(player2)
+#     break if player2.life_points <= 0 # Check if player2 is defeated after player1's attack
+
+#     player2.attacks(player1)
+#     break if player1.life_points <= 0 # Check if player1 is defeated after player2's attack
+#   end
+
+#   and_the_winner_is(player1, player2, player3)
+# end
---
+++ lib/player_2.rb
@@ -0,0 +1,101 @@
+# Classe représentant un bot
+class Player
+  attr_accessor :name, :life_points
+
+  def initialize(name)
+    @name = name
+    @life_points = 10
+  end
+
+  # ---
+
+  def show_state
+    print "¤ #{@name} a actuellement #{@life_points} PVs."
+  end
+
+  # ---
+
+  def gets_damage(damage)
+    @life_points -= damage
+    if @life_points <= 0
+      @life_points = 0
+      puts "¤ #{@name} n'a plus de PVs! Il est complètement DIE."
+    else
+      puts "¤ #{@name} n'a plus que #{@life_points}PVs.\n\n"
+    end
+  end
+
+  # ---
+
+  def attacks(player)
+    puts ">>> #{@name} attaque #{player.name} !"
+    attack_damage = compute_damage
+    puts ">>> #{@name} inflige #{attack_damage} point(s) de dégât(s) à #{player.name}!"
+    player.gets_damage(attack_damage)
+  end
+
+  def compute_damage
+    rand(1..6)
+  end
+end # ferme la classe "Player"
+
+# --- 
+
+# Classe représentant un joueur humain
+class HumanPlayer < Player
+  attr_accessor :current_weapon_level
+
+  def initialize(name)
+    super(name)
+    @life_points = 10
+    @current_weapon_level = 1
+  end
+
+  def show_state
+    print "~ #{@name} a actuellement #{@life_points} PVs et une arme de niveau #{@current_weapon_level}."
+  end
+
+  def compute_damage
+    rand(1..6) * @current_weapon_level
+  end
+
+  # ---
+
+  def search_weapon
+    found_weapon_level = rand(1..6)
+    puts "\nTu cherches une nouvelle arme...\nCherche-cherche-cherche..."
+    puts "\nTu as trouvé une arme de niveau #{found_weapon_level} !"
+
+    if found_weapon_level > @current_weapon_level
+      puts "\nMais elle déchire cette fourchette ! ( •_•)>⌐■-■ (⌐■_■)"
+      puts "Ton arme fatale gagne #{found_weapon_level - @current_weapon_level} dents ! Ça pique sa mère !\n\n"
+      @current_weapon_level = found_weapon_level
+    elsif found_weapon_level == @current_weapon_level
+      puts "\nDamn... elle n'est pas mieux que ton arme actuelle... ლ( `Д’ ლ)\nQue de temps et d'opportunités de perdus..."
+    else
+      puts "\nF@*#{$.}... elle est carrément pourrave cette arme... (╥﹏╥ )\nAllô le game-design ?!"
+    end
+  end
+
+  # ---
+
+  def search_health_pack
+    search_health_pack = rand(1..6)
+    puts "\nTu cherches un pack de PVs... comme si c'était normal...\nCherche-cherche-cherche..."
+
+    if search_health_pack == 1
+      puts "\nTu n'as rien trouvé (╯°□°）╯︵ ┻━┻\nRNGesus, where are you quand je need you ?!\n"
+      # puts "Ton arme fatale gagne #{search_health_pack - @current_weapon_level} dents ! Ça pique sa mère !\n"
+    elsif search_health_pack == 6
+      puts "\nTu as trouvé un pack de +80PVs (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ ! \nT'es vraiment trop fort maggle ! \n"
+    else
+      puts "\nTu as trouvé un pack de +50PVs ┏━┓ ︵ /(^.^/)! \nC'est toujours ça de pris pour un mec lambda ! \n"
+    end
+  end
+
+
+
+
+
+  
+end # ferme la classe "HumanPlayer"
