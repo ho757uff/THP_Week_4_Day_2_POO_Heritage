4dc96b93f6facfde269a57c28c7da32c8d8d502e
ho757uff <robindebessel.pro@gmail.com>
2023-10-17 14:54:47 +0200

    fight flow v1 - finished

--- app.rb
+++ app.rb
@@ -1,7 +1,57 @@
-require 'bundler'
+require "bundler"
 Bundler.require
 
-require_relative 'lib/game'
-require_relative 'lib/player'
+require_relative "lib/game"
+require_relative "lib/player"
 
-binding.pry
+player1 = Player.new("Josi_la_kikoo")
+player2 = Player.new("José_la_malice")
+
+def combat_warmup(player1, player2)
+  puts "\nLadies & Gentlemen! Bienvenue pour ce combat d'anthologie qui opposera...\n"
+  puts "#{player1.name}, l'indétrônable !"
+  puts "VS"
+  puts "#{player2.name}, l'irrésistible !"
+  puts "\nVoici l'état de chaque joueur avant leur affrontement titanesque :\n"
+  puts "#{player1.show_state}"
+  puts "#{player2.show_state}"
+
+  puts "\nNOooOwwW ! Let's get READYYY To RrrRRrRrRRRuuUUMMBLLLLLLLLLLLLLLLLLLLLE !"
+  puts "\n\n"
+end
+
+def round_preview(player1, player2)
+  puts "~~~~~~~~~~~~~~~~~~~~ NEW ROUND ~~~~~~~~~~~~~~~~~~~~"
+  puts "\nVoici l'état de chaque joueur avant le round :"
+  puts "#{player1.show_state}"
+  puts "#{player2.show_state}"
+  puts "\n"
+end
+
+def combat(player1, player2)
+  while player1.life_points > 0 && player2.life_points > 0
+    round_preview(player1, player2)
+
+    player1.attacks(player2)
+    break if player2.life_points <= 0 # Check if player2 is defeated after player1's attack
+
+    player2.attacks(player1)
+    break if player1.life_points <= 0 # Check if player1 is defeated after player2's attack
+  end
+
+  and_the_winner_is(player1, player2)
+end
+
+def and_the_winner_is(player1, player2)
+  if player1.life_points <= 0
+    puts ".#{player2.name} WINS!\n\n"
+  elsif player2.life_points <= 0
+    puts ".#{player1.name} WINS!\n\n"
+  end
+end
+
+# round_preview(player1, player2)
+combat_warmup(player1, player2)
+combat(player1, player2)
+
+# binding.pry
--- lib/player.rb
+++ lib/player.rb
@@ -8,26 +8,28 @@ class Player
   end
   
   def show_state()
-    puts ">> Le joueur #{self.name} a actuellement #{self.life_points}PVs."
+    print "~ Le joueur #{self.name} a actuellement #{self.life_points}PVs."
   end
 
   def gets_damage(damage)
     @life_points = @life_points - damage
     if @life_points <= 0
       @life_points = 0
-      return puts ">> Le joueur #{self.name} n'a plus de PVs! Il a été tué.\n>> Rest In Pieces dear #{self.name} !"
+      return puts "> Le joueur #{self.name} n'a plus de PVs! Il a été tué.\n\n_Rest In Pieces dear ''#{self.name}'' !"
     end
-    puts ">> Le joueur #{self.name} a actuellement #{self.life_points}PVs."
+    puts "> Le joueur #{self.name} n'a plus que #{self.life_points}PVs.\n\n"
   end
 
   def attacks(player)
-    puts ">> #{self.name} attaque #{player.name} !"
+    puts ">>> #{self.name} attaque #{player.name} !"
     attack_damage = compute_damage
-    puts ">> {self.name} inflige #{attack_damage} point(s) de dégât(s) à #{player.name} !"
+    puts ">> #{self.name} inflige #{attack_damage} point(s) de dégât(s) à #{player.name} !"
     player.gets_damage(attack_damage)
   end
 
   def compute_damage
     return rand(1..6)
   end
+
 end #ferme la classe Player
+
